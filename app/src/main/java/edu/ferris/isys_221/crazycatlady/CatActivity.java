256bc1077d905f62377d2e9c0c91263a9d686bbd
Chris Cosnowski <chris.cosnowski@gmail.com>
2018-12-10 03:25:30 -0500

    First Commit

---
+++ app/src/main/java/edu/ferris/isys_221/crazycatlady/CatActivity.java
@@ -0,0 +1,23 @@
+//Name: Chris Cosnowski
+//Date Written: 12/07/2018
+//Instructor: Hira Herrington
+//Course: ISYS 221–VL1 Fully Online
+//Purpose: Final Project - a game of cat snatching!
+//Inputs/Outputs:
+//Exposed Interfaces:
+//Variables/Types:
+
+
+package edu.ferris.isys_221.crazycatlady;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CatActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cat);
+    }
+}
